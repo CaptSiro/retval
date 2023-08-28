47059550472541775441e1700b7f477a26962e51
CaptSiro <honzahorak433@icloud.com>
2023-08-28 18:20:58 +0200

    Actually importing functions fail and success

--- absol_modules/index.php
+++ absol_modules/index.php
@@ -4,3 +4,6 @@ require_once __DIR__ . "/../src/Result.php";
 require_once __DIR__ . "/../src/ResultSet.php";
 
 require_once __DIR__ . "/exc.php";
+
+require_once __DIR__ . "/../src/functions/fail.php";
+require_once __DIR__ . "/../src/functions/success.php";
